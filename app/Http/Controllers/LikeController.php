2f51f94205203310411db275392bd49d4267baf2
Maksix <armageddon4839@gmail.com>
2019-08-28 17:27:20 +0300

    added test like route and validation of partner_id

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Like;
+use App\Profile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,7 +11,11 @@ class LikeController extends Controller
 {
     public function like($partner_id)
     {
-       Like::create(['profile_id' => Auth::id(), 'partner_id' => $partner_id]);
+        if (isset(Profile::where('id', $partner_id)->get()->id)) {
+            Like::create(['profile_id' => Auth::id(), 'partner_id' => $partner_id]);
+        } else {
+            return redirect('/');
+        }
     }
 
     public function getWhoLikedThisProfile()
--- routes/web.php
+++ routes/web.php
@@ -30,5 +30,5 @@ Route::delete('/profiles/{id}', 'ProfileController@destroy');
 
 Route::get('/preferences/create', 'PreferenceController@create')->middleware('verified')->middleware('firstTime');
 Route::post('/preferences', 'PreferenceController@store');
-
+Route::get('/like/{id}', 'LikeController@like');
 
