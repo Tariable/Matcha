21378c0dd4ec2d0a48ccd486096ca189191dc7bb
Maksix <armageddon4839@gmail.com>
2019-09-07 13:50:33 +0300

    updated view

--- resources/views/recommendations/show.blade.php
+++ resources/views/recommendations/show.blade.php
@@ -12,9 +12,10 @@
                 <h1> {{ $data['name'] }}, {{ $data['age'] }}</h1>
             </div>
         </div>
+        <button  class="btn btn-success" style="float:right">Like</button>
+        <button class="btn btn-danger" style="float:left">Dislike</button>
         <div class="card">
             <div class="card-body">
-                <h3>Rating: {{ $data['rating'] }}</h3>
                 <img src="/storage/photos/3ea54fb0e0d9845b3c0c.jpg" alt="" class="img-fluid mb-3">
                 <h3>{{ $data['distance'] }} km from you</h3>
                 <p> {{ $data['desc'] }}</p>
